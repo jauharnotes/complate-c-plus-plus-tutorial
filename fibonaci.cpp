d907efed537ea0794d0f7355ea2f6b6097345ab0
Jauharuddin <jauharnotes@gmail.com>
2021-12-14 20:58:25 +0700

    basic c plus plus

--- fibonaci.cpp
+++ fibonaci.cpp
@@ -7,10 +7,10 @@ int main() {
     int n, f_n, f_n1, f_n2;
 
     cout << "program deret fibonaci" << endl;
-    cout << "masukan nilai ke-n: "; cin >> n;
+    cout << "masukan nilai n: "; cin >> n;
 
-    f_n1 = 1;
-    f_n2 = 0;
+    f_n1 = 0;
+    f_n2 = 1;
     for (int i = 1; i <= n; i++) {
         f_n = f_n1 + f_n2;
         f_n2 = f_n1;
